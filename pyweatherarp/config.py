78b05717313a2947b29c2ee3fcdd6dcd23bf0382
Andrew Piper <pipera@uw.edu>
2018-06-27 10:59:06 -0700

    renamed src -> pyweatherarp

---
+++ pyweatherarp/config.py
@@ -0,0 +1,10 @@
+# Weather update interval in seconds
+UPDATE_INTERVAL = 900
+
+# weather source and api key
+URL = 'openweathermap.org'
+KEY = 'c435dd8b7fd091d8d9cef4fb847c9d7b'
+
+UNITS = 'metric'
+CITY = 'Seattle'
+COUNTRY = 'USA'
